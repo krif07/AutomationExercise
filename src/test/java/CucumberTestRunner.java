22cfa6e73414ffc9165aa92be033125c77d61de0
Cristian Fernando Dávila López <krif07@gmail.com>
2025-03-19 10:29:58 -0500

    Scenarios in parallel in cucumber test runner

--- src/test/java/CucumberTestRunner.java
+++ src/test/java/CucumberTestRunner.java
@@ -1,5 +1,6 @@
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
         features = {"classpath:features"},
@@ -8,4 +9,9 @@ import io.cucumber.testng.CucumberOptions;
         monochrome = true, dryRun = false,
         plugin = {"pretty", "html:target/cucumber.html", "json:target/cucumber.json"})
 public class CucumberTestRunner extends AbstractTestNGCucumberTests {
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
