577caea079e948414084e68ee5acf6f8f6a7354f
Cristian Fernando Dávila López <krif07@gmail.com>
2025-03-22 18:02:27 -0500

    testng xml file and plugin in pom.xml

--- src/test/java/CucumberTestRunner.java
+++ src/test/java/CucumberTestRunner.java
@@ -5,7 +5,7 @@ import org.testng.annotations.DataProvider;
 @CucumberOptions(
         features = {"classpath:features"},
         glue = {"com.automation.exercise.stepdefinitions", "com.automation.exercise.hooks"},
-        tags = "@all",
+        tags = "",
         monochrome = true, dryRun = false,
         plugin = {"pretty", "html:target/cucumber.html", "json:target/cucumber.json"})
 public class CucumberTestRunner extends AbstractTestNGCucumberTests {
