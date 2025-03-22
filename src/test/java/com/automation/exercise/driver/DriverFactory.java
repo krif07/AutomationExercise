626ae4bee198906eb062eb2322fb51742384a5a4
Cristian Fernando Dávila López <krif07@gmail.com>
2025-03-21 19:12:42 -0500

    remote web driver for chrome

--- src/test/java/com/automation/exercise/driver/DriverFactory.java
+++ src/test/java/com/automation/exercise/driver/DriverFactory.java
@@ -8,6 +8,10 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class DriverFactory {
     private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
@@ -16,29 +20,35 @@ public class DriverFactory {
         String browser = ReadProperties.getBrowserType();
         WebDriver webDriver;
 
-        switch (browser.toLowerCase()) {
-            case "firefox" -> {
-                WebDriverManager.firefoxdriver().setup();
-                FirefoxOptions options = new FirefoxOptions();
-                options.addArguments("--headless");
-                webDriver = new FirefoxDriver(options);
-            }
-            case "edge" -> {
-                WebDriverManager.edgedriver().setup();
-                webDriver = new EdgeDriver();
-            }
-            default -> {
-                WebDriverManager.chromedriver().setup();
-                ChromeOptions options = new ChromeOptions();
-                //options.addArguments("--headless");
-                options.addArguments("--no-sandbox");
-                options.addArguments("--disable-dev-shm-usage");
-                webDriver = new ChromeDriver(options);
-            }
+        try{
+            URL seleniumUrl = new URL("http://chrome:4444/wd/hub");
+            switch (browser.toLowerCase()) {
+                case "firefox" -> {
+                    WebDriverManager.firefoxdriver().setup();
+                    FirefoxOptions options = new FirefoxOptions();
+                    options.addArguments("--headless");
+                    webDriver = new FirefoxDriver(options);
+                }
+                case "edge" -> {
+                    WebDriverManager.edgedriver().setup();
+                    webDriver = new EdgeDriver();
+                }
+                default -> {
+                    ChromeOptions options = new ChromeOptions();
+                    //options.addArguments("--headless");
+                    options.addArguments("--no-sandbox");
+                    options.addArguments("--disable-dev-shm-usage");
+                    webDriver = new RemoteWebDriver(seleniumUrl,options);
+                }
 
+            }
+            webDriver.manage().window().maximize();
+            return webDriver;
+
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("Error connecting to Selenium Grid: " + e.getMessage(), e);
         }
-        webDriver.manage().window().maximize();
-        return webDriver;
+
     }
 
     public static void cleanupDriver() {
