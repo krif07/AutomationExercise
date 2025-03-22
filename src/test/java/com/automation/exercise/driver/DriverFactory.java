0bc33561943a10695fb082d2f95e9c1c979ca779
Cristian Fernando Dávila López <krif07@gmail.com>
2025-03-22 18:11:38 -0500

    testng xml file and plugin in pom.xml

--- src/test/java/com/automation/exercise/driver/DriverFactory.java
+++ src/test/java/com/automation/exercise/driver/DriverFactory.java
@@ -35,7 +35,7 @@ public class DriverFactory {
                 case "chrome" -> {
                     WebDriverManager.chromedriver().setup();
                     ChromeOptions options = new ChromeOptions();
-                    //options.addArguments("--headless");
+                    options.addArguments("--headless");
                     options.addArguments("--no-sandbox");
                     options.addArguments("--disable-dev-shm-usage");
                     webDriver = new ChromeDriver(options);
